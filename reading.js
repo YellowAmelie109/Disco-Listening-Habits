558f9f3748569b17ddfc643731617cd556b41c3b
joemoseley-exe-collacuk <joemoseley@exe-coll.ac.uk>
2025-03-11 10:18:17 +0000

    improve reading.ts

---
+++ reading.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+function handleResponse() {
+    var jsonData = fs.readFileSync('JSONS/response.json', 'utf8');
+    var jsonObject = JSON.parse(jsonData);
+    var songs = [];
+    var times_ms = [];
+    var number_of_songs = jsonObject["total"];
+    var artists = [];
+    var explisit_rate = 0;
+    for (var i = 0; i < number_of_songs; i++) { //get the data for each line song that is listed as well as 
+        var song = jsonObject["items"][i];
+        songs.push(song["name"]);
+        times_ms.push(song["duration_ms"]);
+        for (var key in song["artists"]) {
+            if (Object.prototype.hasOwnProperty.call(song["artists"], key)) {
+                var element = song["artists"][key];
+                artists.push(element["name"]);
+            }
+            ;
+        }
+        ;
+        if (song["explicit"]) {
+            explisit_rate = explisit_rate + 1;
+        }
+    }
+    ;
+    explisit_rate = explisit_rate / number_of_songs;
+    var mean_time_ms = 0;
+    for (var key in times_ms) {
+        if (Object.prototype.hasOwnProperty.call(times_ms, key)) {
+            var element = times_ms[key];
+            mean_time_ms = mean_time_ms + element;
+        }
+        ;
+    }
+    ;
+    mean_time_ms = mean_time_ms / number_of_songs;
+    var mean_time_s = mean_time_ms / 1000;
+    return { "songs": songs, "mean_time_s": mean_time_s, "artists": artists, "explisit_rate": explisit_rate };
+}
+//console.log(handleResponse());
--- reading.ts
+++ reading.ts
@@ -1,20 +1,43 @@
 import * as fs from 'fs';
 
-
 function handleResponse(){
-    let songs: string[]
-
     const jsonData = fs.readFileSync('JSONS/response.json', 'utf8');
     const jsonObject = JSON.parse(jsonData);
+    
+    let songs: string[] = []
+    let times_ms: number[] = []
+    let number_of_songs: number = jsonObject["total"]
+    let artists: string[] = []
+    let explisit_rate: number = 0
 
-    for (const key in jsonObject) {
-        if (Object.prototype.hasOwnProperty.call(jsonObject, key)) {
-            const element = jsonObject[key];
-            console.log(element)
+    for (let i = 0; i < number_of_songs; i++){//get the data for each line song that is listed as well as 
+        const song = jsonObject["items"][i];
+        songs.push(song["name"]);
+        times_ms.push(song["duration_ms"]);
+        for (const key in song["artists"]) {
+            if (Object.prototype.hasOwnProperty.call(song["artists"], key)) {
+                const element = song["artists"][key];
+                artists.push(element["name"]);
+            };
+        };
+        if (song["explicit"]){
+            explisit_rate = explisit_rate + 1
+        }
+    };
+    
+    explisit_rate = explisit_rate / number_of_songs
+
+    let mean_time_ms: number = 0
+    for (const key in times_ms) {
+        if (Object.prototype.hasOwnProperty.call(times_ms, key)) {
+            const element = times_ms[key];
+            mean_time_ms = mean_time_ms + element;
         };
     };
-    //console.log(jsonObject);
-    //songs.push()
+    mean_time_ms = mean_time_ms / number_of_songs;
+    let mean_time_s = mean_time_ms/1000
+
+    return {"songs":songs, "mean_time_s":mean_time_s, "artists":artists, "explisit_rate":explisit_rate}
 }
 
-handleResponse()
+//console.log(handleResponse());
