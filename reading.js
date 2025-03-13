0596274df1c98fa0a74b48e03bcb56fe71d9a4f1
joemoseley-exe-collacuk <joemoseley@exe-coll.ac.uk>
2025-03-13 14:45:29 +0000

    reading.ts updated

--- reading.js
+++ reading.js
@@ -39,7 +39,7 @@ function handleResponse(jsonObject) {
     var times_ms = [];
     var number_of_songs = jsonObject["total"];
     var artists = [];
-    var explisit_rate = 0;
+    var explicit_rate = 0;
     for (var i = 0; i < number_of_songs; i++) { //get the data for each line song that is listed as well as 
         var song = jsonObject["items"][i];
         songs.push(song["name"]);
@@ -53,11 +53,11 @@ function handleResponse(jsonObject) {
         }
         ;
         if (song["explicit"]) {
-            explisit_rate = explisit_rate + 1;
+            explicit_rate = explicit_rate + 1;
         }
     }
     ;
-    explisit_rate = explisit_rate / number_of_songs;
+    explicit_rate = explicit_rate / number_of_songs;
     var mean_time_ms = 0;
     for (var key in times_ms) {
         if (Object.prototype.hasOwnProperty.call(times_ms, key)) {
@@ -69,27 +69,27 @@ function handleResponse(jsonObject) {
     ;
     mean_time_ms = mean_time_ms / number_of_songs;
     var mean_time_s = mean_time_ms / 1000;
-    return { "songs": songs, "mean_time_s": mean_time_s, "artists": artists, "explicit_rate": explisit_rate };
+    return { "songs": songs, "mean_time_s": mean_time_s, "artists": artists, "explicit_rate": explicit_rate };
 }
-function getJson(authKey) {
-    function getUserData(authKey) {
-        return __awaiter(this, void 0, void 0, function () {
-            var response;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, fetch("https://api.spotify.com/v1/me", {
-                            method: 'GET',
-                            headers: { 'Authorization': 'Bearer ' + authKey.access_token },
-                        })];
-                    case 1:
-                        response = _a.sent();
-                        return [4 /*yield*/, response.json()];
-                    case 2: return [2 /*return*/, _a.sent()];
-                }
-            });
+function getJsonData(authKey) {
+    return __awaiter(this, void 0, void 0, function () {
+        var response;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fetch("https://api.spotify.com/v1/me", {
+                        method: 'GET',
+                        headers: { 'Authorization': 'Bearer ' + authKey.access_token },
+                    })];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2: return [2 /*return*/, _a.sent()];
+            }
         });
-    }
-    ;
-    var jsonObject = getUserData(authKey);
-    return jsonObject;
+    });
 }
+;
+function main(authKey) {
+    return handleResponse(getJsonData(authKey));
+}
+;
--- reading.ts
+++ reading.ts
@@ -1,9 +1,9 @@
-function handleResponse(jsonObject){
+function handleResponse(jsonObject){//looks at the json data and returns an object with the interresting infomation
     let songs: string[] = []
     let times_ms: number[] = []
     let number_of_songs: number = jsonObject["total"]
     let artists: string[] = []
-    let explisit_rate: number = 0
+    let explicit_rate: number = 0
 
     for (let i = 0; i < number_of_songs; i++){//get the data for each line song that is listed as well as 
         const song = jsonObject["items"][i];
@@ -16,11 +16,11 @@ function handleResponse(jsonObject){
             };
         };
         if (song["explicit"]){
-            explisit_rate = explisit_rate + 1
+            explicit_rate = explicit_rate + 1
         }
     };
     
-    explisit_rate = explisit_rate / number_of_songs
+    explicit_rate = explicit_rate / number_of_songs
 
     let mean_time_ms: number = 0
     for (const key in times_ms) {
@@ -32,20 +32,17 @@ function handleResponse(jsonObject){
     mean_time_ms = mean_time_ms / number_of_songs;
     let mean_time_s = mean_time_ms/1000
 
-    return {"songs":songs, "mean_time_s":mean_time_s, "artists":artists, "explicit_rate":explisit_rate}
+    return {"songs":songs, "mean_time_s":mean_time_s, "artists":artists, "explicit_rate":explicit_rate}
 }
 
-function getJson(authKey){
-    async function getUserData(authKey) {
-        const response = await fetch("https://api.spotify.com/v1/me", {
-          method: 'GET',
-          headers: { 'Authorization': 'Bearer ' + authKey.access_token },
-        });
+async function getJsonData(authKey) {//from app.js
+    const response = await fetch("https://api.spotify.com/v1/me", {
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + authKey.access_token },
+    });
 
-        return await response.json();
-    };
-    
-    let jsonObject = getUserData(authKey);
-
-    return jsonObject
-}
+    return await response.json();
+};
+function main(authKey){
+    return handleResponse(getJsonData(authKey));
+};
