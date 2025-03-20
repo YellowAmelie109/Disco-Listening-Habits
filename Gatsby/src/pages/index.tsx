7b570ee21ab886e317d66ce9f141bfa3be3fe475
2-4-glitch <caidencree@exe-coll.ac.uk>
2025-03-20 14:10:45 +0000

    adding link function import

--- Gatsby/src/pages/index.tsx
+++ Gatsby/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import responseJSON from "./reading"
+import { Link } from "gatsby"
 
 const pageStyles = {
   color: "#232129",
