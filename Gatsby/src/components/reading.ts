6f14732d0ab506058404f2a62532df710306ec3e
joemoseley-exe-collacuk <joemoseley@exe-coll.ac.uk>
2025-03-27 15:05:45 +0000

    Update reading.ts

--- Gatsby/src/components/reading.ts
+++ Gatsby/src/components/reading.ts
@@ -65,6 +65,9 @@ async function getSonginfo(songs : string[][]) {
             const song = songs[key];
             
             const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=track.gettoptags&artist=".concat(song[0], "&track=", song[1],"&api_key=", "64773b9be5304689ec27ead878787c92", "&format=json"))
+            console.log(response)
+            console.log(response["toptags"])
+            console.log(response["toptags"]["tag"])
             console.log(response["toptags"]["tag"][0])
             //songData.push([song[0], song[1], ])
         }
