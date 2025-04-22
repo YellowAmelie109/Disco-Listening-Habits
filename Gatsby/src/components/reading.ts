30680887dba0bb402b42161a34ac4085d2b5ddfc
Amelie <amelie.machin109@gmail.com>
2025-04-22 19:47:59 +0100

    Pie chart now displays genres from user data

--- Gatsby/src/components/reading.ts
+++ Gatsby/src/components/reading.ts
@@ -1,19 +1,22 @@
 async function handleResponse(jsonObject : any){//looks at the json data and returns an object with the interresting infomation
-    let songs: string[] = []
+    let songs = []
     let times_ms: number[] = []
-    let number_of_songs: number = await jsonObject["total"]
-    let artists: string[] = []
+    let number_of_songs: number = Object.keys(await jsonObject["items"]).length
+    let artists = []
     let explicit_rate: number = 0
 
     for (let i = 0; i < number_of_songs; i++){//get the data for each line song that is listed as well as 
         const song = await jsonObject["items"][i];
         songs.push(await song["name"]);
         times_ms.push(await song["duration_ms"]);
+        let element = []
         for (const key in await song["artists"]) {
             if (Object.prototype.hasOwnProperty.call(await song["artists"], key)) {
-                const element = await song["artists"][key];
-                artists.push(await element["name"]);
+                element.push(await song["artists"][key]["name"]);
             };
+        };
+        if(element){
+            artists.push(element);
         };
         if (await song["explicit"]){
             explicit_rate = explicit_rate + 1
@@ -48,12 +51,11 @@ async function getJsonData(authKey: string|null) {//from app.js gets the json fr
 
 export async function main(/**authKey: string|null*/){
     let songInfo = await handleResponse(await getJsonData(window.localStorage.getItem('token')));
-
-    let songs: string[][] = [] //gets the list of songs
+    let songs = [] //gets the list of songs
     for (const key in await songInfo["songs"]) {
         if (Object.prototype.hasOwnProperty.call(songInfo["songs"], key)) {
             const element = [songInfo["songs"][key], songInfo["artists"][key]];
-            songs.push(await element);
+            songs.push(element);
         };
     };
 
@@ -64,21 +66,24 @@ export async function main(/**authKey: string|null*/){
 
 async function getSonginfo(songs: any[]) {//pings audioscrobbler to find the song genr5a (if listed)
     var songData: string[][] = []
-    
-    for (const key in songs) {
+    for (let key in songs) {
         if (Object.prototype.hasOwnProperty.call(songs, key)) {
             const song = songs[key];
         
-            const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=track.gettoptags&artist=".concat(song[1], 
-                "&track=", song[0],
+            const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=track.gettoptags&artist=".concat(encodeURIComponent(song[1][0]), 
+                "&track=", encodeURIComponent(song[0]),
                 "&api_key=", "64773b9be5304689ec27ead878787c92",
                 "&format=json"), {
                 method: 'GET',
             });
             
             let currentData = await response.json()
-            songData.push([await currentData["toptags"]["@attr"]["artist"], await currentData["toptags"]["@attr"]["track"], await currentData["toptags"]["tag"][0]["name"]])
-       }
+            if (await currentData["toptags"] && await currentData["toptags"]["tag"] && await currentData["toptags"]["tag"][0]){
+                songData.push([await song[1].join(", "), await song[0], await currentData["toptags"]["tag"][0]["name"]])
+            }else{
+                songData.push([await song[1].join(", "), await song[0], "Unknown"])
+            }
+        };
     }
     return await songData;
 }
